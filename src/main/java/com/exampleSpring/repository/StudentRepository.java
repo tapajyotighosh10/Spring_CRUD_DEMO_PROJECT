c415ab3c72554c9138f90c9192cc46de28178438
Tapajyoti Ghosh <57596257+tapajyotighosh10@users.noreply.github.com>
2025-02-20 15:48:55 +0530

    upsert implemented

--- src/main/java/com/exampleSpring/repository/StudentRepository.java
+++ src/main/java/com/exampleSpring/repository/StudentRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student,Long> {
     @Query(value = "SELECT * FROM student WHERE student_name = ?1", nativeQuery = true)
-    List<Student> getStudentsByName(String studentName);
+    Optional<Student> getStudentsByName(String studentName);
+
+    Optional<Student> findByEmail(String email);
 }
